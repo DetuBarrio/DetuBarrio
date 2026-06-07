161aeecce4f073590ab69bacb9d91da519c1fac8
jjcastilla94 <alvarocastilla49@gmail.com>
2026-06-07 18:28:35 +0200

    bug con delete comercio

--- backend/src/main/java/detubarrio/rest/model/Disponibilidad.java
+++ backend/src/main/java/detubarrio/rest/model/Disponibilidad.java
@@ -35,7 +35,6 @@ public class Disponibilidad {
 
     // 🛠️ NUEVA RELACIÓN CORREGIDA: Borrado en cascada para limpiar las reservas de esta disponibilidad
     @OneToMany(mappedBy = "disponibilidad", cascade = CascadeType.ALL, orphanRemoval = true, fetch = FetchType.LAZY)
-    @OneToMany(mappedBy = "idDisponibilidad", cascade = CascadeType.ALL, orphanRemoval = true, fetch = FetchType.LAZY)
     @JsonIgnore
     private List<Reserva> reservas;
 }
